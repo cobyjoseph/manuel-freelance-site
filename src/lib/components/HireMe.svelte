8c5441f346854adaefcfb5c1603fdd6cce0daf75
Coby Joseph <coby.joseph@gmail.com>
2023-02-08 19:51:26 -0300

    hire me cloud improveed

--- src/lib/components/HireMe.svelte
+++ src/lib/components/HireMe.svelte
@@ -1,32 +1,40 @@
-<div class="grid grid-cols-1 sm:grid-cols-2 widthClamp shadow-xl bg-white bg-opacity-70 rounded-xl">
+<div
+	class="flex flex-col sm:grid sm:grid-cols-2 justify-center  widthClamp shadow-xl bg-white bg-opacity-70 rounded-xl"
+>
 	<!-- LEFT COLUMM -->
-	<div class="flex flex-col gap-6 p-8">
+	<div class="flex flex-col sm:grid  gap-6 p-8">
 		<div class=" text-med">WHY HIRE ME?</div>
 		<div class=" text-big">You know it's time to outsource content writing.</div>
 		<div class=" text-thin">More text description about why to hire me goes here.</div>
 	</div>
 
 	<!-- RIGHT COLUMN -->
-	<img src="icons/cloud-papers.png" alt="graphic of papers flying around" class="" />
+	<div class="p-3 flex justify-center items-center sm:grid">
+		<img
+			src="icons/cloud.png"
+			alt="graphic of papers flying around"
+			class="p-3 -mt-[2.5rem] sm:mt-0 picWidthClamp"
+		/>
+	</div>
 </div>
 
 <style>
 	.text-med {
-		font-size: clamp(0.8rem, 4vw, 1.5rem);
+		font-size: clamp(1rem, 2vw, 1.5rem);
 		line-height: 1.08;
 		color: #8c8c8c;
 		font-weight: 800;
 	}
 
 	.text-big {
-		font-size: clamp(1rem, 4vw, 2.5rem);
+		font-size: clamp(1.35rem, 3vw, 2.5rem);
 		line-height: 1.08;
 		color: #282a30;
 		font-weight: 700;
 	}
 
 	.text-thin {
-		font-size: clamp(0.8rem, 4vw, 1.5rem);
+		font-size: clamp(1.1rem, 2vw, 1.5rem);
 		line-height: 1.08;
 		color: #8c8c8c;
 		font-weight: 300;
@@ -34,5 +42,9 @@
 
 	.widthClamp {
 		width: clamp(10rem, 90vw, 55rem);
+	}
+
+	.picWidthClamp {
+		width: clamp(20rem, 20vw, 45rem);
 	}
 </style>
